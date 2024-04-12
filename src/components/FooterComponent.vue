f011eb8d733417b255c47cf1b99f8ef528197f6f
samuelescavello <scavellosamuele23@gmail.com>
2024-04-12 11:27:08 +0200

    footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <h1>ciao sono il footer</h1>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name:'FooterComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
